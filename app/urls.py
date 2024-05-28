d32ad97868f3b30a004e4efc9eeba1e6ee576045
Luiz henrique <luizpq33@gmail.com>
2024-05-28 19:38:13 -0400

    Feito o CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from generos.views import genero_view
+from generos.views import genero_create_list_view,genero_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('generos/', genero_view, name='genero-list'),
+    path('generos/', genero_create_list_view, name='genero'),
+    path('generos/<int:pk>/', genero_detail_view, name='detail-genero')
 ]
--- generos/views.py
+++ generos/views.py
@@ -1,7 +1,38 @@
+import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from generos.models import Genero
 
-def genero_view(request):
-    generos = Genero.objects.all()
-    data = [{'id': genero.id, 'nome': genero.nome} for genero in generos]
-    return JsonResponse(data, safe=False)
+@csrf_exempt
+def genero_create_list_view(request):
+    if request.method == 'GET':
+        generos = Genero.objects.all()
+        data = [{'id': genero.id, 'nome': genero.nome} for genero in generos]
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        novo_genero = Genero(nome=data['nome'])
+        novo_genero.save()
+        return JsonResponse({'id': novo_genero.id, 'nome': novo_genero.nome}, status=201)
+    
+@csrf_exempt
+def genero_detail_view(request, pk):
+    genero = get_object_or_404(Genero, pk=pk)
+
+    if request.method == 'GET':
+        data = {'id': genero.id, 'nome': genero.nome}
+        return JsonResponse(data)
+    
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genero.nome = data['nome']
+        genero.save()
+        return JsonResponse({'id': genero.id, 'nome': genero.nome}, status=201)
+
+    elif request.method == 'DELETE':
+        genero.delete()
+        return JsonResponse(
+            {'mensagem': 'Gênero excluido com sucesso!'},
+            status=204,
+        )
