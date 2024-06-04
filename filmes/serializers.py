f50eecabcdafe321089a5cbc00d5f8f3d1883d00
Luiz henrique <luizpq33@gmail.com>
2024-06-03 22:09:53 -0400

    Campos calculados

--- filmes/serializers.py
+++ filmes/serializers.py
@@ -2,6 +2,23 @@ from rest_framework import serializers
 from filmes.models import Filme
 
 class FilmesSerializer(serializers.ModelSerializer):
+    media = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Filme
         fields = '__all__'
+
+    
+    def get_media(self, obj):
+        reviews = obj.filme_review.all() 
+        if not reviews:
+            return 0
+        media = sum([review.estrelas for review in reviews]) / reviews.count()
+        return round(media, 2)
+
+
+    def validate_data_lancamento(self, value):
+        if value.year < 1964:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990')
+        else:
+            return value
